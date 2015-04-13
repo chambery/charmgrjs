935792d60150c55cb5cb0c7c617686ab78d9216c
Todd Chambery <todd.chambery@gmail.com>
2015-04-13 13:44:54 -0400

    wip

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
+var sqlite3 = require("sqlite3").verbose();
 var express = require('express'),
-    app = express.createServer();
-var fs = require('fs');
+    app = express();
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
 
-function make_dir(path) {
-    try {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride());
+/* errs */
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+var dateFormat = require('dateformat');
+var fs = require('fs');
+var file = "../propmgmt.sqlite";
+var exists = fs.existsSync(file);
+console.log("file exists: " + exists);
+var db = new sqlite3.Database(file);
+console.log("db: " + db.toString());
+YAML = require('yamljs');
+
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+
+function make_dir(path)
+{
+    try
+    {
         fs.lstatSync(path);
-    } catch (e) {
+    } catch (e)
+    {
         fs.mkdirSync(path, 0755);
     }
 }
 
-app.use(express.bodyParser());
-app.use(express.logger());
 
 app.get('/character/:owner/:name', function (req, res) {
     console.log("in get character");
     fs.readFile("users/" + req.params.owner + "/" + req.params.name + "/chardata", encoding = 'utf8', function (err, data) {
         if (err) {
-        	res.send(err, 404);
+            res.send(err, 404);
             console.log(err);
         } else {
             res.send(data);
@@ -30,7 +70,7 @@ app.get('/log/:owner/:name/:entry_id', function (req, res) {
     console.log("in get log entry");
     fs.readFile("users/" + req.params.owner + "/" + req.params.name + "/" + req.params.entry_id, encoding = 'utf8', function (err, data) {
         if (err) {
-        	res.send(err, 404);
+            res.send(err, 404);
             console.log(err);
         } else {
             res.send(data);
@@ -45,23 +85,23 @@ app.get('/delete/:owner/:name', function (req, res) {
     console.log("reading file");
     fs.readFile("users/" + req.params.owner + "/" + req.params.name + "/chardata", encoding = 'utf8', function (err, data) {
         if (err) {
-        	res.send(err, 404);
+            res.send(err, 404);
             console.log(err);
         } else {
-        	console.log("Successfully read")
+            console.log("Successfully read")
             chardata = data;
             console.log(data);
             console.log("renaming file");
             fs.rename("users/" + req.params.owner + "/" + req.params.name + "/chardata", "users/" + req.params.owner + "/" + req.params.name + "/chardata_" + parseInt((new Date()).getTime(), 16), function (err) {
-				if (err) {
-					res.send(err, 404);
-					console.log(err);
-				} else {
-					console.log("Successfully renamed")
-					res.send(chardata);
-					console.log(chardata);
-				}
-			});	
+                if (err) {
+                    res.send(err, 404);
+                    console.log(err);
+                } else {
+                    console.log("Successfully renamed")
+                    res.send(chardata);
+                    console.log(chardata);
+                }
+            });
         }
     });
 });
@@ -73,7 +113,7 @@ app.post('/character/:owner/:name', function (req, res) {
     make_dir("users/" + req.params.owner + "/" + req.params.name);
     fs.writeFile("users/" + req.params.owner + "/" + req.params.name + "/chardata", JSON.stringify(req.body), 'utf8', function (err) {
         if (err) {
-        	res.send(err, 500);
+            res.send(err, 500);
             console.log(err);
         } else {
             console.log("The file was saved!");
@@ -90,7 +130,7 @@ app.post('/log/:owner/:name', function (req, res) {
     make_dir("users/" + req.params.owner + "/" + req.body.char_name);
     fs.writeFile("users/" + req.params.owner + "/" + req.body.char_name + "/" + req.body.id, JSON.stringify(req.body), 'utf8', function (err) {
         if (err) {
-        	res.send(err, 500);
+            res.send(err, 500);
             console.log(err);
         } else {
             console.log("The file was saved!");
@@ -100,9 +140,9 @@ app.post('/log/:owner/:name', function (req, res) {
 });
 
 app.use('/', express.static(__dirname));
-app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-}));
+
 app.listen(1860);
 console.log('Express server started on port 1860');
+
+
+
